fcfb4205355beca6bfa6304c79db91236ddca345
Belmin <belminpobric99@gmail.com>
2022-05-08 23:18:32 +0200

    unused things

--- FoodKing/FoodKing.Services/BaseService.cs
+++ FoodKing/FoodKing.Services/BaseService.cs
@@ -1,10 +1,5 @@
 ﻿using AutoMapper;
 using FoodKing.Services.Database;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FoodKing.Services
 {
--- FoodKing/FoodKing.Services/IService.cs
+++ FoodKing/FoodKing.Services/IService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FoodKing.Services
+﻿namespace FoodKing.Services
 {
     public interface IService<T> where T : class
     {
