2a39dfe0fa1cfbc0f14fcea044fc6b1c3bc02810
Belmin Pobric <belminpobric99@gmail.com>
2025-05-23 13:23:37 +0200

    dailyMenu crud

---
+++ FoodKing/Controllers/DailyMenuController.cs
@@ -0,0 +1,22 @@
+﻿using FoodKing.Model;
+using FoodKing.Model.Requests;
+using FoodKing.Model.SearchObjects;
+using FoodKing.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodKing.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DailyMenuController : BaseCRUDController<DailyMenu, DailyMenuSearchObject, DailyMenuInsertRequest, DailyMenuUpdateRequest>
+    {
+        private readonly IDailyMenuService _service;
+        private readonly ILogger<DailyMenuController> _logger;
+
+        public DailyMenuController(ILogger<DailyMenuController> logger, IDailyMenuService service) : base(logger, service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+    }
+}
